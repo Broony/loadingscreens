f28afdfdff80c458decfa84912ccaac5834bc034
josh <broony620@gmail.com>
2018-06-27 01:59:21 +0100

    loadingscreen

---
+++ config.php
@@ -0,0 +1,9 @@
+<?php
+
+ /* An XML database can easily be read, therefore, private information should be set here */
+	
+	$user = "admin"; // username
+	$passw = "jbroon123"; // password
+	$apikey = "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"; // Find your API Key at https://steamcommunity.com/dev/apikey
+
+?>
